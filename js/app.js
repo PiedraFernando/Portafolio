ecabf7b04fee3f999bd20b6900eb879935d74747
Fernando Piedra <fernandopiedram@gmail.com>
2022-09-26 14:05:47 -0500

    change tecnologis

--- js/app.js
+++ js/app.js
@@ -1,38 +1,24 @@
-const imgs = document.getElementsByTagName('img')
+const imgs = document.getElementsByTagName('img');
 const cargarImg = (entradas, observador) => {
-    entradas.forEach(entrada => {
-        if(entrada.isIntersecting){
-            entrada.target.classList.add('visible')
-        }else{
-            entrada.target.classList.remove('visible')
-        }
-    });
-}
+  entradas.forEach((entrada) => {
+    if (entrada.isIntersecting) {
+      entrada.target.classList.add('visible');
+    } else {
+      entrada.target.classList.remove('visible');
+    }
+  });
+};
+
 const observador = new IntersectionObserver(cargarImg, {
-    root:null,
-    rootMargin: '0px',
-    threshold: 0.6,
-})
-for(const img of imgs){
-    observador.observe(img)
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.6,
+});
+for (const img of imgs) {
+  observador.observe(img);
 }
 
-function openProyect(cityName) {
-    var i;
-    var x = document.getElementsByClassName("proyectos");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(cityName).style.display = "block";
-
-    var x = document.getElementsByClassName("folderLi");
-    for (i = 0; i < x.length; i++) {
-        x[i].classList.remove("folderSelected")
-        console.log(x[i])
-    }
-    document.getElementById(cityName+"Li").classList.add("folderSelected");
-}
-setTimeout(() => {
-    let loader = document.getElementById('loading')
-    loader.style.display = 'none'
-}, 5000);
+onload = () => {
+  let loader = document.getElementById('loading');
+  loader.style.display = 'none';
+};
